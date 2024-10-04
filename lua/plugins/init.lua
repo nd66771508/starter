95bcbed4cbf8dd2c4e93e1badeac22ca87a09906
Manh Pham <manhp7658@gmail.com>
2024-10-04 13:01:57 +0700

    update markdown-preview.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -133,11 +133,15 @@ return {
   -- 'nvim-telescope/telescope.nvim',
   -- }
   },
+
 {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
 },
 
   {
